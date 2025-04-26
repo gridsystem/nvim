dd5a5eedcba15ef78ce6d2a2bd5042be47ff01d4
Tom Burchill <2365737+gridsystem@users.noreply.github.com>
2025-04-26 02:05:11 +0100

    feat: Add indent-blankline

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,12 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        -- char = '│', -- character for spaces (soft indent)
+        tab_char = '→', -- character specifically for tabs
+        smart_indent_cap = false, -- when enabled, caps the number of indentation levels by looking at the surrounding code
+      },
+    },
   },
 }
