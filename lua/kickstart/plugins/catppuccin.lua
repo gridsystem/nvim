12ceb8ad0416f5ea57c772eb4a478d31c63f33c4
Tom Burchill <2365737+gridsystem@users.noreply.github.com>
2025-04-30 22:39:00 +0100

    feat: Configure catppuccin theme options

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -11,18 +11,58 @@ return {
       require('catppuccin').setup {
         flavour = 'mocha',
         transparent_background = true,
-        show_end_of_buffer = true,
+        show_end_of_buffer = false,
         term_colors = true,
         dim_inactive = {
           enabled = true, -- dims the background color of inactive window
           shade = 'dark',
           percentage = 0.5, -- percentage of the shade to apply to the inactive window
         },
+        integrations = {
+          blink_cmp = true,
+          fidget = true,
+          gitsigns = true,
+          indent_blankline = {
+            enabled = true,
+            colored_indent_levels = true,
+          },
+          mason = true,
+          mini = {
+            enabled = true
+          },
+          neotree = true,
+          native_lsp = {
+            enabled = true,
+            virtual_text = {
+              errors = { "italic" },
+              hints = { "italic" },
+              warnings = { "italic" },
+              information = { "italic" },
+              ok = { "italic" },
+            },
+            underlines = {
+              errors = { "underline" },
+              hints = { "underline" },
+              warnings = { "underline" },
+              information = { "underline" },
+              ok = { "underline" },
+            },
+            inlay_hints = {
+              background = true,
+            },
+          },
+          treesitter = true,
+          telescope = {
+            enabled = true,
+            -- style = "nvchad"
+          },
+          which_key = true
+        }
       }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.background = dark
+      vim.cmd.background = 'dark'
       vim.opt.termguicolors = true
       vim.cmd.colorscheme 'catppuccin'
     end,
