d3c950287b466c529e525c36d9080bbc8d11c4e0
Tom Burchill <2365737+gridsystem@users.noreply.github.com>
2025-04-30 22:39:49 +0100

    feat: Configure indent-blankline

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -5,11 +5,28 @@ return {
     -- See `:help ibl`
     main = 'ibl',
     opts = {
-      indent = {
-        -- char = '│', -- character for spaces (soft indent)
-        tab_char = '→', -- character specifically for tabs
-        smart_indent_cap = false, -- when enabled, caps the number of indentation levels by looking at the surrounding code
-      },
     },
+    config = function()
+      local highlight = {
+        'RainbowRed',
+        'RainbowYellow',
+        'RainbowBlue',
+        'RainbowOrange',
+        'RainbowGreen',
+        'RainbowViolet',
+        'RainbowCyan',
+      }
+      require('ibl').setup {
+        indent = {
+          -- char = '│', -- character for spaces (soft indent)
+          tab_char = '→', -- character specifically for tabs
+          smart_indent_cap = false, -- when enabled, caps the number of indentation levels by looking at the surrounding code
+          highlight = highlight,
+        },
+        scope = {
+          enabled = false,
+        },
+      }
+    end
   },
 }
