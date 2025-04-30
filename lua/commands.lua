93b6e79a11970170d75f8657e1501f8b57ba59bf
Tom Burchill <2365737+gridsystem@users.noreply.github.com>
2025-04-30 22:45:38 +0100

    feat: Save cursor position when closing a file and restore when next opening

--- lua/commands.lua
+++ lua/commands.lua
@@ -101,3 +101,14 @@ vim.api.nvim_create_autocmd("BufEnter", {
   end,
 })
 
+-- Auto-save last cursor position and restore it
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"') -- last cursor position
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+      vim.cmd("normal! zz") -- center cursor in viewport
+    end
+  end,
+})
