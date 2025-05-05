8b67d07810814f5b2ab81bab15d437c2bf2c1e77
gridsystem <2365737+gridsystem@users.noreply.github.com>
2025-05-05 18:32:36 +0100

    feat: Add CSS language server and ensure web dev treesitter parsers

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -270,8 +270,15 @@ return {
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
+        'cssls',
         'stylua', -- Used to format Lua code
       })
+
+      require('lspconfig').cssls.setup {
+        capabilities = capabilities,
+        filetypes = { 'css', 'scss', 'less' },
+      }
+
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
       require('mason-lspconfig').setup {
